58feb0c2795a77f4de667f793a4cd74ff3b2e676
EugeneShipachev <32467380+EugeneShipachev@users.noreply.github.com>
2017-10-06 20:27:09 -0700

    Add files via upload

---
+++ EugeneFirstWeek/src/MyStory.java
@@ -0,0 +1,26 @@
+/*10/5/2017*/
+
+public class MyStory {
+  public static void main(String [] args) {
+	  
+ System.out.println("Hi.");/* 'Hi'-line is necessary*/
+ System.out.println();/*separated from other text*/
+ System.out.println("Please find my short story below:");/*Introducing*/
+ System.out.println();//One more line to make the text look better. Below the text goes as usual:
+ System.out.print("  I was born in Kazakhstan. ");
+ System.out.println("In 2012 I've moved to US with my family. ");
+ System.out.print("Now I'm working at T-Mobile as STE. ");
+ System.out.println("I like my job, but I would like to grow in my carrier.");
+ System.out.println("What's why I decided to take Java-Selenium classes this year. \r\n" + /*here I'm trying "\r\n +" */
+   "I like the class coz. I can learn Git also and even more stuff. It's more than expected! \r\n" +
+   "By the way, I've learned my first programming language in 1993 when I was 15 y/o. And it was Basic. \r\n" +
+   "I was able to create multicolored and different sizes moving objects;) ");
+ System.out.println();/*The reader can get bored already, so better for me to stop here*/
+ System.out.println();
+ System.out.println("Thank you.");
+ System.out.println("Sincerely,");
+ System.out.println("Yevgeniy Shipachev.");
+
+  
+  }
+}
